9ef2b953f9384315554c11f0dd69dad80dd13d0b
ZeddHp <valters.jargans@gmail.com>
2024-05-31 17:28:42 +0300

    full repo

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\View\View;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('posts.index', [
+            'posts' => Post::with('user')->latest()->paginate(10),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+            'image' => 'nullable|image|max:2048', // Validates that the file is an image and its size does not exceed 2MB.
+        ]);
+
+        $imagePath = null;
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+            $fullPath = $request->file('image')->store('posts', 'public');
+            $imagePath = basename($fullPath); // Extracting filename
+        }
+
+        $post = $request->user()->posts()->create([
+            'message' => $validated['message'],
+            'image_path' => $imagePath // Ensuring this is the filename only
+        ]);
+
+        return redirect(route('posts.index'));
+    }
+
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post): View
+    {
+        Gate::authorize('update', $post);
+
+        return view('posts.edit', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post): RedirectResponse
+    {
+        Gate::authorize('update', $post);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $post->update($validated);
+
+        return redirect(route('posts.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post): RedirectResponse
+    {
+        Gate::authorize('delete', $post);
+
+        $post->delete();
+
+        return redirect(route('posts.index'));
+    }
+}
